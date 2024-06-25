e7c727e0cd76ddf6bfd5a27a27b91eccf3893f3c
Joao Matheus <j.matheusr@hotmail.com>
2024-06-25 20:33:40 -0300

    Initial Commit

---
+++ cats-dogs/cats_dogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cats_dogsApp.swift
+//  cats-dogs
+//
+//  Created by Joao Rocha on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct cats_dogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
