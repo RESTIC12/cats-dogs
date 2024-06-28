47c3b6d9344c89d4b81e71dcc86b5662deea189f
Joao Matheus <j.matheusr@hotmail.com>
2024-06-28 19:01:41 -0300

    feat: implementing LazyVGrid to show dogs images.

--- cats-dogs/Dogs/View/DogsView.swift
+++ cats-dogs/Dogs/View/DogsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DogsView: View {
     @StateObject var viewModel = DogsViewModel()
-    
+
     let columns = [
         GridItem(.adaptive(minimum: 100, maximum: 200)),
         GridItem(.adaptive(minimum: 100, maximum: 200))
@@ -19,14 +19,15 @@ struct DogsView: View {
         ScrollView(.vertical) {
             if let dogs = viewModel.dogs {
                 LazyVGrid(columns: columns, spacing: 10) {
-                    ForEach(dogs.message, id: \.self) { element in
-                        AsyncImage(url: URL(string: element)) { result in
-                            result.image?
-                                .resizable()
-                                .scaledToFit()
+                    Section(header: Text("Dogs")) {
+                        ForEach(dogs.message, id: \.self) { element in
+                            AsyncImage(url: URL(string: element)) { result in
+                                result.image?
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                            .cornerRadius(8)
                         }
-                        .cornerRadius(8)
-//                        .frame(width: 200, height: 200)
                     }
                 }
                 .padding(.horizontal, 8)
